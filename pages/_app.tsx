3831dababd5126a8218a1a80599bbb9ec4e0155d
Elliot <elliot.janvier@epitech.eu>
2024-02-28 17:56:13 +0100

    [ADD] added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,14 @@ import "@/styles/weatherpage.css";
 import "@/styles/weeklyforecast.css";
 import "@/styles/othercities.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
