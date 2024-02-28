e01c83c0dc5abe387a72a0afe368581f8445cbc1
Elliot <elliot.janvier@epitech.eu>
2024-02-28 17:59:49 +0100

    [ADD] added vercel speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import "@/styles/weeklyforecast.css";
 import "@/styles/othercities.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
