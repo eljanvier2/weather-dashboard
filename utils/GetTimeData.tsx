4a85f50740146a0fedc7385e94327878d433340f
Elliot <elliot.janvier@epitech.eu>
2024-03-04 15:20:12 +0100

    [FIX] removed gitignore from utils/

---
+++ utils/GetTimeData.tsx
@@ -0,0 +1,19 @@
+export const getHourMinutes = (dateString: string): string => {
+  const date = new Date(dateString)
+  return date.getHours() + ':' + date.getMinutes()
+}
+
+export const getDay = (dateString: string): string => {
+  const date = new Date(dateString)
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ]
+  const dayName = days[date.getDay()]
+  return dayName
+}
